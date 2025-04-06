fc8ff7986a2fe8bea4b4f4cccf8d3fe8a3078246
Mofazzal-Hossain-Evan <md.mofazzalhossain18@gmail.com>
2025-04-06 20:58:04 +0600

    Normal Stack Example

---
+++ My_Practices/Section 4/19/src/ds/stack/app.java
@@ -0,0 +1,18 @@
+package ds.stack;
+
+public class app {
+    public static void main(String[] args) {
+
+        stack theStack = new stack(10);
+        theStack.push(23);
+        theStack.push(25);
+        theStack.push(29);
+        theStack.push(22);
+
+
+while (!theStack.isEmpty()){
+    long value = theStack.pop();
+    System.out.println(value);
+}
+    }
+}
