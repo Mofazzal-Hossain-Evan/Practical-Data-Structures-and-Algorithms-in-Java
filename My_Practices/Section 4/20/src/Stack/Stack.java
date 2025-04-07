b6f13d6554a8935d879c715b7c2ef6bd1754d596
Mofazzal-Hossain-Evan <md.mofazzalhossain18@gmail.com>
2025-04-07 19:18:49 +0600

    LIFO (Last In, First Out) principle

---
+++ My_Practices/Section 4/20/src/Stack/Stack.java
@@ -0,0 +1,47 @@
+public class Stack {
+
+    private int maxSize;
+    private long[] stackArray;
+    private int top;
+
+
+    public Stack(int Size ) {
+        this.maxSize = Size;
+        this.stackArray = new long[maxSize];
+        this.top = -1;
+    }
+
+    public void push(long j){
+
+        if (isFull()) {
+            System.out.println("this stack is full");
+        } else {
+            top++;
+            stackArray[top] = j;
+        }
+    }
+
+    public long pop(){
+
+        if (isEmpty()){
+            System.out.println("the stack is empty");
+            return -1;
+        } else {
+            int old_top = top;
+            top--;
+            return stackArray[old_top];
+        }
+    }
+
+    public long peak(){
+        return stackArray[top];
+    }
+
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+
+    public boolean isFull(){
+        return (maxSize-1 == top);
+    }
+}
